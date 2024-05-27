fb4fdc979ab542d156d2bd557380475bba3763d1
Yahia <yn7599@gmail.com>
2024-05-28 01:21:31 +0300

    7 is insane

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,7 +1,5 @@
 export default function getBudgetObject(income, gdp, capita) {
-  return {
-    income,
-    gdp,
-    capita,
-  };
+  const budget = { income, gdp, capita };
+
+  return budget;
 }
