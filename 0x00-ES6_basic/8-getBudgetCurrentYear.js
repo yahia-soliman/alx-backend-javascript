cf330a78bbfcc9f23f858e5383aa828a2fcf9627
Yahia <yn7599@gmail.com>
2024-05-28 01:16:56 +0300

    typos

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,10 +4,11 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const year = getCurrentYear();
   const budget = {
-    [`income-${getCurrentYear()}`]: income,
-    [`gdb-${getCurrentYear()}`]: gdp,
-    [`capita-${getCurrentYear()}`]: capita,
+    [`income-${year}`]: income,
+    [`gdp-${year}`]: gdp,
+    [`capita-${year}`]: capita,
   };
 
   return budget;
