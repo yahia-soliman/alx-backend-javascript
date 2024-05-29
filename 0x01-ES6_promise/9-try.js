29f2f07412246dd1ef178ce725477fedae51154a
Yahia <yn7599@gmail.com>
2024-05-29 18:53:46 +0300

    fix task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,5 +1,10 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  queue.push(mathFunction(), 'Guardrail was processed');
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(`${err.name}: ${err.message}`);
+  }
+  queue.push('Guardrail was processed');
   return queue;
 }
