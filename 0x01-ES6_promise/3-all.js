a98371cb2875dfd937e294c32d7512304320d2e8
Yahia <yn7599@gmail.com>
2024-05-29 19:14:39 +0300

    fix task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default async function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then((all) => {
     console.log(`${all[0].body} ${all[1].firstName} ${all[1].lastName}`);
-  });
+  }).catch(() => console.log('Signup system offline'));
 }
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,18 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const results = [];
-
-  try {
-    const user = await signUpUser(firstName, lastName);
-    results.push({ status: 'fulfilled', value: user });
-
-    const photo = await uploadPhoto(fileName);
-    results.push({ status: 'fulfilled', value: photo });
-  } catch (err) {
-    results.push({ status: 'rejected', value: err });
-  }
-
-  return results;
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((all) => all.map((one) => ({ status: 'fulfilled', value: one })));
 }
