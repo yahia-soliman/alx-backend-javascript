26cb2d146ae686ba1b2163bb7129e4001c1740cc
Yahia <yn7599@gmail.com>
2024-07-04 01:59:43 +0300

    task_2 typescript

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,24 +1,43 @@
-interface Student {
-  firstName: string;
-  lastName: string;
-  age: number;
-  location: string;
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
 }
 
-const student1: Student = {
-  firstName: 'Ali',
-  lastName: 'Alocka',
-  age: 22,
-  location: 'Attaba',
-};
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
 
-const student2: Student = {
-  firstName: 'Ashraf',
-  lastName: 'Kohka',
-  age: 20,
-  location: 'Mahalla',
-};
+class Director implements DirectorInterface {
+  workFromHome(): string { return 'Working from home'; }
+  getCoffeeBreak(): string { return 'Getting a coffee break'; }
+  getToWork(): string { return 'Getting a coffee break'; }
+  workDirectorTasks(): string { return 'Getting to director tasks'; }
+}
 
-const studentsList: Student[] = [student1, student2];
+class Teacher implements TeacherInterface {
+  workFromHome(): string { return 'Cannot work from home'; }
+  getCoffeeBreak(): string { return 'Cannot have a break'; }
+  workTeacherTasks(): string { return 'Getting to work'; }
+}
 
-console.table(studentsList, ['firstName', 'location']);
+export function createEmployee(salary: number | string): Teacher | Director {
+  return salary < 500 ? new Teacher() : new Director();
+}
+
+export function isDirector(employee: Director | Teacher): boolean {
+  return employee instanceof Director;
+}
+
+export function executeWork(employee: Director | Teacher): string {
+  if (employee instanceof Director) return employee.workDirectorTasks();
+  if (employee instanceof Teacher) return employee.workTeacherTasks();
+}
+
+type Subjects = 'Math' | 'History';
+
+export function teachClass(todayClass: Subjects): string {
+  return `Teaching ${todayClass}`;
+}
