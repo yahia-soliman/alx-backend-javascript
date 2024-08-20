5252bfd27c808c69f004a59defb2816eab19a9c7
Yahia <yn7599@gmail.com>
2024-08-20 10:19:50 +0300

    update read_csv flow

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -6,13 +6,17 @@ function countStudents(path) {
     const lines = data.split('\n');
     const fieldIdx = lines[0].split(',').findIndex((o) => o === 'field');
     const studentsByField = {};
-    console.log(`Number of students: ${lines.length - 1}`);
+    let n = 0;
     for (let i = 1; i < lines.length; i += 1) {
       const student = lines[i].split(',');
       const field = student[fieldIdx];
-      if (!studentsByField[field]) studentsByField[field] = [];
-      studentsByField[field].push(student[0]);
+      if (field) {
+        if (!studentsByField[field]) studentsByField[field] = [];
+        studentsByField[field].push(student[0]);
+        n += 1;
+      }
     }
+    console.log(`Number of students: ${n}`);
     for (const field in studentsByField) {
       if (studentsByField[field]) {
         const list = studentsByField[field];
