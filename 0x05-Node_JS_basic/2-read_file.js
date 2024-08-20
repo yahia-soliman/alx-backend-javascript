6161dc979f6697f1ccb507e686db4b92674a308f
Yahia <yn7599@gmail.com>
2024-08-20 12:15:37 +0300

    2-read_file is sync

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,29 +1,35 @@
 const fs = require('fs');
 
+function displayStudents(data) {
+  const lines = data.split('\n');
+  const fieldIdx = lines[0].split(',').findIndex((o) => o === 'field');
+  const studentsByField = {};
+  let n = 0;
+  for (let i = 1; i < lines.length; i += 1) {
+    const student = lines[i].split(',');
+    const field = student[fieldIdx];
+    if (field) {
+      if (!studentsByField[field]) studentsByField[field] = [];
+      studentsByField[field].push(student[0]);
+      n += 1;
+    }
+  }
+  if (n) console.log(`Number of students: ${n}`);
+  for (const field in studentsByField) {
+    if (studentsByField[field]) {
+      const list = studentsByField[field];
+      console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
+    }
+  }
+}
+
 function countStudents(path) {
-  fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
-    if (err) throw new Error('Cannot load the database');
-    const lines = data.split('\n');
-    const fieldIdx = lines[0].split(',').findIndex((o) => o === 'field');
-    const studentsByField = {};
-    let n = 0;
-    for (let i = 1; i < lines.length; i += 1) {
-      const student = lines[i].split(',');
-      const field = student[fieldIdx];
-      if (field) {
-        if (!studentsByField[field]) studentsByField[field] = [];
-        studentsByField[field].push(student[0]);
-        n += 1;
-      }
-    }
-    if (n) console.log(`Number of students: ${n}`);
-    for (const field in studentsByField) {
-      if (studentsByField[field]) {
-        const list = studentsByField[field];
-        console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
-      }
-    }
-  });
+  try {
+    const data = fs.readFileSync(path, { encoding: 'utf8' });
+    displayStudents(data);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
