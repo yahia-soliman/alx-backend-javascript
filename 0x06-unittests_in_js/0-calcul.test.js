b2736d5c782541e747bff814be86fc188c4737ff
Yahia <yn7599@gmail.com>
2024-08-24 15:34:03 +0300

    update: new test for calcul

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,4 +1,3 @@
-const { describe, it } = require("mocha");
 const assert = require("assert");
 const calculateNumber = require("./0-calcul.js");
 
@@ -6,5 +5,6 @@ describe("calculate sum of 2 numbers", () => {
   it("1 + 3 = 4", () => assert.strictEqual(calculateNumber(1, 3), 4));
   it("1 + 3.7 = 5", () => assert.strictEqual(calculateNumber(1, 3.7), 5));
   it("1.2 + 3.7 = 5", () => assert.strictEqual(calculateNumber(1.2, 3.7), 5));
+  it("1.7 + 3.2 = 5", () => assert.strictEqual(calculateNumber(1.7, 3.2), 5));
   it("1.5 + 3.7 = 6", () => assert.strictEqual(calculateNumber(1.5, 3.7), 6));
 });
